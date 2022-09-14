9bc86b83d110bc9acb185d6cdaa8c8e8916afa49
Daniel <danielbeatoramirez@gmail.com>
2022-09-14 16:26:00 -0300

    develop

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello umbnb_mobile</Text>
+      <Text>Hello umbnb_mobile!</Text>
       <StatusBar style="auto" />
     </View>
   );
