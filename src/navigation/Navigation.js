f1d1a527fd04b95b5eb5fb09179ebb3c288c84f0
Lucas Ollarce <luk5ollarce@gmail.com>
2022-10-05 22:18:00 -0300

    Hide HomeScreen header

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -27,6 +27,7 @@ function MyTabs() {
                     tabBarIcon: ({ color, size }) => (
                         <MaterialCommunityIcons name="home" size={size} color={color} />
                     ),
+                    headerShown: false,
                 }}
             />
             <Tab.Screen
