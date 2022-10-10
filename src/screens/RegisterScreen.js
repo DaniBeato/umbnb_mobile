ecfa981a2dd15535e28c641ddb5f9953b0c716a7
Lucas Ollarce <luk5ollarce@gmail.com>
2022-10-10 04:27:51 -0300

    update register screen

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,18 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { View, ScrollView } from 'react-native';
+import Text from '../components/Text';
+import Logo from '../components/Logo';
+import Input from '../components/TextInput';
+import Button from '../components/Button';
 
 const RegisterScreen = () => {
   return (
-    <View>
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Logo
+        marginTop = '80px'
+      />
       <Text
+        title='Create an account to start planning your next trip.'
+        color='gray'
+        fontSize='15px'
+        marginTop='0px'
+      />
+      <Input placeholder="First Name" />
+      <Input placeholder="Last Name" />
+      <Input placeholder="Age" />
+      <Input placeholder="Email" />
+      <Input placeholder="Phone" />
+      <Input placeholder="Sex" />
+      <Input placeholder="Province" />
+      <Input placeholder="Host" />
+      <Input placeholder="Password" secureTextEntry={true} />
+      <Button
+        onPress={() => alert('Register')}
+        width='80%'
+        title="Sign Up"
+      />
+      <View
         style={{
-          fontSize: 30,
-          textAlign: "center",
-          marginTop: 50
-        }}
-      >Register Screen</Text>
-    </View>
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          width: '100%',
+          marginTop: 20,
+          marginBottom: 20
+        }}>
+        <Text
+          title='Already have an account?'
+          color='gray'
+        />
+        <Button
+          onPress={() => alert('Login')}
+          bgColor='white'
+          border='1px'
+          marginTop='10px'
+          width='30%'
+          color='#000'
+          title="Log in"
+        />
+      </View>
+    </ScrollView>
   );
-  }
+}
 
 export default RegisterScreen;
